19a7538d9358d8dde3f5710f430f510fd871591c
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2021-12-02 19:06:11 +0200

    Add clear timeouts

--- js/script_j.js
+++ js/script_j.js
@@ -5,18 +5,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const alert = document.getElementById('alert')
     let isJumping = false
     let isGameOver = true
+    let returned = true
 
     function control(e) {
         if (e.keyCode === 32) {
+            console.log("returned: " + returned)
             if (!isJumping && isGameOver === false) {
                 isJumping = true
                 jump()
             }
-            if(isGameOver === true){
+            if (isGameOver === true /* && returned === true */) {
+                console.log("start")
+                returned = false;
+                document.querySelectorAll('.obstacle').forEach(e => e.remove());
                 isGameOver = false;
-                console.log(isGameOver)
+                // console.log(isGameOver)
+                // clearTimeout(generateObstacles)
                 generateObstacles()
                 jump()
+                alert.innerHTML = ''
             }
         }
     }
@@ -29,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
         position = 1
         // let count = 0
         let timerId = setInterval(function () {
-            
+
             //move down
             if (speed < 4) {
                 gravity = 1
@@ -42,12 +49,12 @@ document.addEventListener('DOMContentLoaded', () => {
                     // }
                     // position -= 30
                     // count--
-                    gravity = gravity*1.02
-                    speed = speed*gravity
+                    gravity = gravity * 1.02
+                    speed = speed * gravity
                     position = position - speed
-                    if(position > 1)
+                    if (position > 1)
                         dino.style.bottom = position + 'px'
-                    else{
+                    else {
                         dino.style.bottom = 1 + 'px'
                         clearInterval(downTimerId)
                         isJumping = false
@@ -59,39 +66,85 @@ document.addEventListener('DOMContentLoaded', () => {
             // position += 30
             // count++
             // speed = speed / gravity
-            gravity = gravity/1.02
-            speed = speed*gravity
+            gravity = gravity / 1.02
+            speed = speed * gravity
             position = position + speed
             // console.log("speed" + speed)
             // console.log("position" + position)
-            if(position > 1)
+            if (position > 1)
                 dino.style.bottom = position + 'px'
         }, 20)
     }
+    let timeout = 0
 
     function generateObstacles() {
-        let randomTime = Math.random() * 4000
-        console.log("random time: " + randomTime)
-        let obstaclePosition = 1000
-        const obstacle = document.createElement('div')
-        if (!isGameOver) obstacle.classList.add('obstacle')
-        grid.appendChild(obstacle)
-        obstacle.style.left = obstaclePosition + 'px'
-
-        let timerId = setInterval(function () {
-            if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
-                clearInterval(timerId)
-                alert.innerHTML = 'Game Over'
-                isGameOver = true
-                //remove all children
-                // body.removeChild(body.firstChild)
-                // while (grid.firstChild) {
-                //     grid.removeChild(grid.lastChild)
-                // }
-            }
-            obstaclePosition -= 2
+        if (isGameOver === false) {
+            // console.log("spawn")
+            let randomTime = Math.random() * 4000
+            // console.log("random time: " + randomTime)
+            let obstaclePosition = 1000
+            const obstacle = document.createElement('div')
+            obstacle.classList.add('obstacle')
+            grid.appendChild(obstacle)
             obstacle.style.left = obstaclePosition + 'px'
-        }, 1)
-        if (!isGameOver) setTimeout(generateObstacles, randomTime)
+
+            if (isGameOver === false) {
+                let timerId = setInterval(function () {
+                    if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+                        clearInterval(timerId)
+                        // obstaclePosition = 1000
+                        alert.innerHTML = 'Game Over'
+                        isGameOver = true
+                        clearTimeout(timeout)
+                        // console.log("game over")
+                        //remove all children
+                        // body.removeChild(body.firstChild)
+                        // while (grid.firstChild) {
+                        //     grid.removeChild(grid.lastChild)
+                        // }
+                        console.log("test")
+                        return
+                    }
+                    if (!isGameOver) {
+                        obstaclePosition -= 2
+                        obstacle.style.left = obstaclePosition + 'px'
+                        // console.log(clearTimeout(generateObstacles))
+                    } else {
+                        console.log("test")
+                        console.log("timeout" + timeout)
+                        clearTimeout(timeout)
+                        obstaclePosition = 1000
+                        clearInterval(timerId)
+                        return
+                        // clearInterval(generateObstacles)
+                    }
+                    // console.log("obstaclePosition: " + obstaclePosition)
+                }, 1)
+            } else return
+            if (isGameOver === false) timeout = setTimeout(generateObstacles, randomTime)
+            else {
+                console.log(clearTimeout(generateObstacles))
+                clearTimeout(timeout)
+            }
+        } else {
+            clearTimeout(timeout)
+            obstaclePosition = 1000
+            console.log("return")
+            returned = true
+            return true
+        }
+        // if (!isGameOver) setTimeout(generateObstacles, randomTime)
+        // else{
+        //     console.log("return")
+        //     return
+        // }
     }
+
+    // function checker() {
+    //     setTimeout(checker, 1)
+    //     if(isGameOver === true)
+    //         clearTimeout(generateObstacles)
+    //     console.log("test")
+    // }
+    // checker()
 })
