1ec932266a053f16db66fac54e6d32b89e570579
KatjaDa <end.of.all89@gmail.com>
2021-12-15 12:29:35 +0200

    made map responsive, added map styles and pictures

--- js/script_k.js
+++ js/script_k.js
@@ -10,8 +10,8 @@ if(i.src== "https://robohash.org/robo"||i.src=='https://users.metropolia.fi/~kat
     i.bln = !i.bln; 
 }
 
-if(i.src== "https://robohash.org/robo2"||i.src=="https://robohash.org/robo6"){
-    i.src = i.bln ? "https://robohash.org/robo2" : "https://robohash.org/robo6";
+if(i.src== "https://robohash.org/robo2"||i.src=="https://users.metropolia.fi/~katjadah/mediakurssin-palautukset/j_kuva.jpg"){
+    i.src = i.bln ? "https://robohash.org/robo2" : "https://users.metropolia.fi/~katjadah/mediakurssin-palautukset/j_kuva.jpg";
     i.bln = !i.bln;
 }
 }
